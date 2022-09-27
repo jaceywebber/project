b64ef82a1fa247e46b97bc4334865db227be6584
Jacey Webber <jacey.webber@selu.edu>
2022-09-27 20:02:09 +0000

    function to select and filter data

---
+++ scripts/select_and_filter.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+data <- read_csv("../data/surveys.csv")
+
+select_and_filter <- function(dataset = data){
+  new_data <- data %>% 
+    filter(years > 1997) %>% 
+    select(ButterflySpecies, year, Springtemp)
+    return(new_data)
+    
+}
+
