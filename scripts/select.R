1602837150b68a1cda3dbe87ee477c663cc4902f
Jacey Webber <jacey.webber@selu.edu>
2022-09-27 20:20:57 +0000

    select species

--- scripts/select.R
+++ scripts/select.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
-data <- read_csv("../data/butterfly_data.csv")
+data <- read_csv("../data/surveys.csv")
 
-select_species <- function(dataset = data){
+select_data <- function(dataset = data){
   new_data <- data %>% 
     select(ButterflySpecies, year, Springtemp)
     return(new_data)
